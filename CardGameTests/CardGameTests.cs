0cbaa7eb3517992b1ff60d58638961eabc7dcb0e
Iason Tsotoulidis <47227611+tsotoul@users.noreply.github.com>
2020-11-25 04:03:01 +0200

    Add files via upload

---
+++ CardGameTests/CardGameTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingChallenge.CardGame;
+
+namespace CardGameTests
+{
+    [TestClass]
+    public class CardGameTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //ICard Expected = new Card(Suit.Spades, Value.King);
+
+            //ICard removedCard = ANewPack.TakeCardFromTopOfPack();
+
+
+        }
+    }
+}
