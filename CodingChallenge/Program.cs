988fe92bd6deabc169967d01b002b1a993e16766
Iason Tsotoulidis <47227611+tsotoul@users.noreply.github.com>
2020-11-25 04:05:25 +0200

    Add files via upload

---
+++ CodingChallenge/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodingChallenge.ReversingString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringUtilities StringUtilities = new StringUtilities();
+            String Result = StringUtilities.Reverse("Hello Foo");
+            Console.WriteLine(Result);
+        }
+    }
+}
---
+++ CodingChallenge/StringUtilities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CodingChallenge.ReversingString
+{
+    public class StringUtilities
+    {
+        public static string Reverse(string s)
+        {
+            char[] result = new char[s.Length];
+
+            for(int i = 0; i <= s.Length / 2; i++)
+            {
+                result[i] = s[s.Length - i - 1];
+                result[s.Length - i - 1] = s[i];                
+            }
+            return new String(result);
+            //return result.ToString() throws 'System char[]' in the console
+        }
+    }
+}
